e88857ea60e80b401e430acfe027544aa65af8a1
Mateo <mateo.nicoloff25@gmail.com>
2024-02-01 22:32:52 -0300

    integracion de base de datos

--- backend/controllers/controllersInmuebles.js
+++ backend/controllers/controllersInmuebles.js
@@ -1,8 +1,56 @@
+knex = require('../config/database')
+
+
 const bienvenida = (req, res) => {
     res.send("Bienvenidos a INMUEBLES SRL")
 }
 
+const verTodos = async (req, res) => {
+    const result = await knex('inmobiliaria').select('*')
+    res.json(result)
+}
+
+const buscarPorId = async (req, res) => {
+    const id = req.params.id
+    const result = await knex('inmobiliaria').select('*')
+                                            .where('id', "=" ,id)
+    if (result.length != 0){
+        res.json(result)
+    } else {
+        res.status(404).send(`No se encontró ningún inmueble con el ID ${id}`);
+    }
+} 
+
+const crearInmueble = async (req, res) => {
+    const body = req.body
+    const result = await knex('inmobiliaria').insert(body).returning('*')
+    res.json(result)
+}
+
+const editarInmueble = (req, res) => {
+    res.send("Editar Inmueble")
+}
+
+const eliminarInmueble = (req, res) => {
+    res.send("Eliminar Inmueble")
+}
+
+const filtrarInmueble = (req, res) => {
+    res.send("Filtar")
+}
+
+const mostrarInfo = (req, res) => {
+    res.send("Info")
+}
+
 module.exports = {
-    bienvenida
+    bienvenida,
+    verTodos,
+    buscarPorId,
+    crearInmueble,
+    editarInmueble,
+    eliminarInmueble,
+    filtrarInmueble,
+    mostrarInfo
 }
 
--- backend/routes/routesInmuebles.js
+++ backend/routes/routesInmuebles.js
@@ -5,18 +5,18 @@ const controllersInmuebles = require("../controllers/controllersInmuebles");
 
 route.get('/', controllersInmuebles.bienvenida);
 
-route.get('/api/inmuebles');
+route.get('/api/inmuebles', controllersInmuebles.verTodos);
 
-route.get('/api/inmuebles/:id');
+route.get('/api/inmuebles/:id', controllersInmuebles.buscarPorId);
 
-route.post('/api/inmuebles/nuevo');
+route.post('/api/inmuebles/nuevo', controllersInmuebles.crearInmueble);
 
-route.put('/api/inmuebles/editar/:id'); 
+route.put('/api/inmuebles/editar/:id', controllersInmuebles.editarInmueble); 
 
-route.delete('/api/inmuebles/eliminar/:id');
+route.delete('/api/inmuebles/eliminar/:id', controllersInmuebles.eliminarInmueble);
 
-route.get('/api/inmuebles/filtro');
+route.get('/api/inmuebles/filtro', controllersInmuebles.filtrarInmueble);
 
-route.get('/api/inmuebles/info');
+route.get('/api/inmuebles/info', controllersInmuebles.mostrarInfo);
 
 module.exports = route;
