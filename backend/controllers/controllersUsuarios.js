fc677da51d690c7b14f31ef824c226ecfaa2d1cc
Mateo <mateo.nicoloff25@gmail.com>
2024-02-06 08:57:04 -0300

    funcion registrar usuario

--- backend/controllers/controllersUsuarios.js
+++ backend/controllers/controllersUsuarios.js
@@ -1,11 +1,22 @@
-
+const knex = require('../config/database')
+const bcrypt = require('bcrypt')
 
 const login = (req, res) => {
     res.send("login")
 }
 
-const registrarUsuario = (req, res) => {
-    res.send("registro")
+const registrarUsuario = async (req, res) => {
+    const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.password, salt);
+
+    const usuario = {
+        username: req.body.username,
+        password: password,
+        id_permisos: req.body.id_permisos
+    }
+
+    const result = await knex('usuarios').insert(usuario).returning('*')
+    res.json(result)
 }
 
 const modificarUsuario = (req, res) => {
